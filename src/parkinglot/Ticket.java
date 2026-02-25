4ac88e5a0f118f37f310096eeb797545c5cb9069
Dipanshuk1 <dipanshukumar1936@gmail.com>
2026-02-25 11:51:27 +0530

    Implemented parkinglot strategy

--- src/parkinglot/Ticket.java
+++ src/parkinglot/Ticket.java
@@ -1,4 +1,20 @@
 package parkinglot;
 
+import vehicles.Vehicle;
+
+import java.time.LocalDateTime;
+
 public class Ticket {
+    private ParkingSpot parkingSpot;
+    private Vehicle vehicle;
+    private LocalDateTime startTime;
+
+
+    // Constructor to initialize ticket with parking spot and vehicle
+    public Ticket(ParkingSpot parkingSpot, Vehicle vehicle) {
+        this.parkingSpot = parkingSpot;
+        this.vehicle = vehicle;
+        this.startTime = LocalDateTime.now();  // Set current time as the start time
+    }
+
 }
